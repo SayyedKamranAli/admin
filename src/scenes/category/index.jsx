45de0e8732a7d3dce579040a06f420da2ecde53e
Sayyed Kamran Ali <kamranali976@gmail.com>
2023-07-27 14:42:11 +0530

    remove dependencies

--- src/scenes/category/index.jsx
+++ src/scenes/category/index.jsx
@@ -8,7 +8,6 @@ import {
 } from "@mui/material";
 import { DataGrid, GridToolbarExport } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
